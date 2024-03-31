321dfefbc53dd818eb9a7ad07b3f9f98f7d42aa6
VigneshReddyJulakanti <73946941+VigneshReddyJulakanti@users.noreply.github.com>
2024-03-31 19:08:39 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCgEm4OCdBLq5OmDNUf2xTbzvpCKY1Zhz0",
-    authDomain: "gatepass-bc959.firebaseapp.com",
-    projectId: "gatepass-bc959",
-    storageBucket: "gatepass-bc959.appspot.com",
-    messagingSenderId: "613667425450", 
-    appId: "1:613667425450:web:1fcd152a12f0aed6d2d4d3"
+    apiKey: "",
+    authDomain: "gatepass-.firebaseapp.com",
+    projectId: "gatepass-",
+    storageBucket: "gatepass-.appspot.com",
+    messagingSenderId: "", 
+    appId: "1::web:"
   };
  
 
